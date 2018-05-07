7e98d87aca69438c546af78a7a7504c340900900
YOU <youwg@suninfo.com>
2018-05-07 12:27:55 +0800

    Dome完成

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const os = require('os')
 const fs = require('fs')
+const path = require('path')
 const Koa = require('koa2')
 const KoaRouter = require('koa-router')
 const bodyParser = require('koa-body')
@@ -11,49 +12,83 @@ app.use(bodyParser({multipart: true}))
 app.use(serve('static'))
 
 let fileMd5List = []
-let files = {}
+let tmpDir = path.join(__dirname, `./uploadTmp`)
+let uploadDir = path.join(__dirname, `./upload/`)
 
 router.post('/uploadChunk', async (ctx, next) => {
-  const md5 = ctx.request.body.md5
+  const md5 = ctx.request.body.fields.md5
   // chunk总数
-  const total = ctx.request.body.total
+  const total = parseInt(ctx.request.body.fields.total)
   // 当前chunk
-  const current = ctx.request.body.current
+  const current = parseInt(ctx.request.body.fields.current)
   // file
-  console.log(ctx.request.body.file)
-  const file = Buffer.from(ctx.request.body.file)
-  if (current !== total) {
-    const reader = fs.createReadStream(file);
-    const stream = fs.createWriteStream(`./uploadTmp/${md5}/${current}`);
-    reader.pipe(stream);
-    ctx.body = {
-      status: true,
-      current: current + 1
+  const file = ctx.request.body.files.file
+  console.log(md5, current, total)
+  if (current < total) {
+    let tmpPath = path.join(tmpDir, md5)
+    if (!fs.existsSync(tmpPath)) {
+      fs.mkdirSync(tmpPath)
     }
+    const reader = fs.createReadStream(file.path);
+    const stream = fs.createWriteStream(path.join(tmpPath, current + ''));
+    reader.pipe(stream);
+    ctx.body = await new Promise(function (resolve, reject) {
+      reader.on('close', function () {
+        resolve({
+          status: true,
+          current: current + 1
+        })
+      })
+    })
   } else {
-    // 上传完毕
+    ctx.body = {
+      status: false,
+      error: '文件分片出错!'
+    }
   }
 })
-router.post('/upload', async (ctx, next) => {
-  console.log(ctx.request.body.files)
-  const file = ctx.request.body.files.file;
-  const reader = fs.createReadStream(file.path);
-  const stream = fs.createWriteStream(path.join('./', Math.random().toString() + file.name));
-  reader.pipe(stream);
-  console.log('uploading %s -> %s', file.name, stream.path);
-  ctx.body = ctx.request.body
-})
+
 router.post('/checkFile', async (ctx, next) => {
   let md5 = ctx.request.body.md5
   if (fileMd5List.indexOf(md5) !== -1) {
-    ctx.body = {
-      status: true
-    }
-  } else {
+    // 文件存在
     ctx.body = {
       status: false
     }
+  } else {
+    try {
+      fs.accessSync(path.join(tmpDir, md5))
+      // 断点续传
+      let files = fs.readdirSync(path.join(tmpDir, md5))
+      console.log(files)
+      ctx.body = {
+        status: true,
+        current: files.length
+      }
+    } catch (e) {
+      ctx.body = {
+        status: true,
+        current: 0
+      }
+    }
   }
+})
+router.post('/finishUpload', async (ctx, next) => {
+  let name = ctx.request.body.name
+  let total = ctx.request.body.total
+  let md5 = ctx.request.body.md5
+  let tmpPath = path.join(tmpDir, md5)
+  let filePath = path.join(uploadDir, md5 + name)
+  fileMd5List.push(md5)
+  for (let i = 0; i < total; i++) {
+    let content = fs.readFileSync(path.join(tmpPath, i + ''))
+    fs.appendFileSync(filePath, content)
+  }
+  ctx.body = {
+    status: true,
+    data: '上传成功!'
+  }
+  console.log('文件写入完成！')
 })
 
 app.use(router.routes()).use(router.allowedMethods())
